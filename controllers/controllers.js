3f760e1628d18307f6e1da4a73909d7c2feac700
felix.535210052 <111833692+felixfren@users.noreply.github.com>
2022-12-05 08:20:09 +0700

    controller model

---
+++ controllers/controllers.js
@@ -0,0 +1,28 @@
+ const axios = require("axios");
+ var env = require('dotenv').config()
+
+var HOST = process.env.DB_URL + "/api";
+
+function getProducts() {
+    return axios
+        .get(HOST + "/getproducts")
+        .then((data) => response.data)
+        .catch((error) => console.log(error));
+}
+
+
+function getProduct(_id) {
+    return axios
+        .get(HOST + "/getproduct", {
+            params: {
+                pid: pid
+            }
+        })
+        .then((response) => response.data)
+        .catch((error) => console.log(error));
+}
+
+module.exports = {
+    getProduct,
+    getProducts
+}
---
+++ models/product.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+  pid: {
+	type: Number,
+  },
+  name: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+	category: {
+		type: String,
+	},
+  image: {
+    type: String,
+  },
+  variety: [		
+    {
+			size_id: String,
+			size: String,
+			stock: Number,
+    },
+    {
+			size_id: String,
+			size: String,
+			stock: Number,
+			//required: true
+    },
+		{
+			size_id: String,
+			size: String,
+			stock: Number,
+			//required: true
+		}
+  ]
+});
+
+const product = mongoose.model("Product", productSchema);
+
+module.exports = product;
